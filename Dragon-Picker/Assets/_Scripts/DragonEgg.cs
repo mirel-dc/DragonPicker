fcc8610bd961eb4f9504862cadf8042eca056ba7
Mirel <valerymirel@mail.ru>
2023-11-07 20:13:06 +0500

    VId #14

--- Dragon-Picker/Assets/_Scripts/DragonEgg.cs
+++ Dragon-Picker/Assets/_Scripts/DragonEgg.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DragonEgg : MonoBehaviour
 {
     public static float bottomY = -30f;
+    public AudioSource audioSource;
 
     void Start()
     {
@@ -21,6 +22,9 @@ public class DragonEgg : MonoBehaviour
         Renderer rend;
         rend = GetComponent<Renderer>(); 
         rend.enabled = false;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
     }
 
     void Update()
